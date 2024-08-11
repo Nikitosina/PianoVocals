ba2f11e1a2c879e8a6dbb2be76f0b01cf14c65d4
nikitarat <nikitarat1@gmail.com>
2024-08-12 01:11:37 +1000

    Initial Commit

---
+++ PianoVocals/PianoVocalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoVocalsApp.swift
+//  PianoVocals
+//
+//  Created by Nikita Ratashnyuk on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PianoVocalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
